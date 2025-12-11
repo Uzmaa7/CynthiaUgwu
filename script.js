18f96b9065057012ec8d7ea724ca4ebcfeef2df1
Uzmaa7 <uk9971khan@gmail.com>
2025-12-11 20:40:01 +0530

    smooth scrolling done

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+//  smooth scrolling
+//  attach locomotive scroll CSS
+//  attach locomotive scroll min js
+//  some code from loco githb for js
+
